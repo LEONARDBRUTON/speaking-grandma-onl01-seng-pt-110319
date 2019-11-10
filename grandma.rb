79912cf57cb0b72f5ccd22c619089b6ea52851ff
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-10 20:35:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@
 def speak_to_grandma(phrase)
   #phrase = "".upcase && "I LOVE YOU GRANDMA!"
   
-  if phrase == phrase.downcase
+  if phrase == phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
